af0bbf321c4f573b17f00ca0a28e3c4d3b64b307
danya-pixel <exisionmonkey@gmail.com>
2022-04-13 23:00:07 +0700

    cnn update

--- 3_CNN/model.py
+++ 3_CNN/model.py
@@ -2,39 +2,34 @@ import numpy as np
 
 from layers import (
     FullyConnectedLayer, ReLULayer,
-    ConvolutionalLayer, MaxPoolingLayer, Flattener,
+    Conv2D, MaxPoolingLayer, Flattener,
     softmax_with_cross_entropy, l2_regularization
     )
 
 
 class AlexNet:
     """
-    Implements a very simple conv net
-    Input -> Conv[3x3] -> Relu -> Maxpool[4x4] ->
-    Conv[3x3] -> Relu -> MaxPool[4x4] ->
-    Flatten -> FC -> Softmax
+    Implements a AlexNet
     """
     def __init__(self, input_shape, n_output_classes):
         """
         Initializes the neural network
         Arguments:
         input_shape, tuple of 3 ints - image_width, image_height, n_channels
-                                         Will be equal to (32, 32, 3)
+                                         Will be equal to (1, 28, 28)
         n_output_classes, int - number of classes to predict
-        conv1_channels, int - number of filters in the 1st conv layer
-        conv2_channels, int - number of filters in the 2nd conv layer
         """
-        self.conv_1 = ConvolutionalLayer(in_channels = input_shape, out_channels = 3, filter_size = 11, padding=4)
+        self.conv_1 = Conv2D(in_channels = input_shape, out_channels = 8, filter_size = 11, stride=4)
         self.relu_1 = ReLULayer()
         self.pool_1 = MaxPoolingLayer(pool_size = 3, stride = 2)
-        self.conv_2 = ConvolutionalLayer(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
+        self.conv_2 = Conv2D(in_channels = 3, out_channels = 8, filter_size = 5, padding = 2)
         self.relu_2 = ReLULayer()
         self.pool_2 = MaxPoolingLayer(pool_size = 3, stride = 2)
-        self.conv_3 = ConvolutionalLayer(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
+        self.conv_3 = Conv2D(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
         self.relu_3 = ReLULayer()
-        self.conv_4 = ConvolutionalLayer(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
+        self.conv_4 = Conv2D(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
         self.relu_4 = ReLULayer()
-        self.conv_5 = ConvolutionalLayer(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
+        self.conv_5 = Conv2D(in_channels = 3, out_channels = 3, filter_size = 5, padding = 2)
         self.relu_5 = ReLULayer()
         self.pool_3 = MaxPoolingLayer(pool_size = 3, stride = 2)
         self.flat = Flattener()
